e8b384aabf514b52a4a5228d2b4f109ad5fc0556
Steve Kuo <steve.kuo@makerstudios.com>
2018-02-06 08:58:07 -0800

    Updated .gitignore to ignore src/github.com and src/golang.org

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	fmt.Printf("hello, world \n")
 	fmt.Printf(stringutil.Reverse("hello, world") + "\n")
-	var str = "This is to test eclipse's variables display\n"
-	fmt.Printf(str)
+	var str string = "This is to test eclipse's variables display\n"
+	shortVarDec := "Lose var and type"
+	fmt.Printf("%s %s\n", str, shortVarDec)
 }
