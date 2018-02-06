1ae117982df228ddf8e5e17270d05ac895dd4735
Steve Kuo <steve.kuo@makerstudios.com>
2018-02-06 09:37:12 -0800

    Updated const_test.go

--- src/hello/constant/const_test.go
+++ src/hello/constant/const_test.go
@@ -1,19 +1,30 @@
 package constant
 
 import (
-    "fmt"
     "testing"
 )
 
 func TestConstant(t *testing.T) {
-	fmt.Printf("StarHyperGiant = %v [%T]\n", StarHyperGiant, StarHyperGiant)
-	fmt.Printf("StarSuperGiant = %v [%T]\n", StarSuperGiant, StarSuperGiant)
-	fmt.Printf("StarBrightGiant = %v [%T]\n", StarBrightGiant, StarBrightGiant)
-	fmt.Printf("StarGiant = %v [%T]\n", StarGiant, StarGiant)
-	fmt.Printf("StarSubGiant = %v [%T]\n", StarSubGiant, StarSubGiant)
-	fmt.Printf("StarDwarf = %v [%T]\n", StarDwarf, StarDwarf)
-	fmt.Printf("StarSubDwarf = %v [%T]\n", StarSubDwarf, StarSubDwarf)
-	fmt.Printf("StarWhiteDwarf = %v [%T]\n", StarWhiteDwarf, StarWhiteDwarf)
-	fmt.Printf("StarRedDwarf = %v [%T]\n", StarRedDwarf, StarRedDwarf)
-	fmt.Printf("StarBrownDwarf = %v [%T]\n", StarBrownDwarf, StarBrownDwarf)
+    
+    cases := []struct {
+		in, want int32
+	}{
+		{2, StarHyperGiant},
+		{4, StarSuperGiant},
+		{8, StarBrightGiant},
+		{16, StarGiant},
+		{32, StarSubGiant},
+		{128, StarDwarf},
+		{256, StarSubDwarf},
+		{512, StarWhiteDwarf},
+		{1024, StarRedDwarf},
+		{2048, StarBrownDwarf},
+	}
+
+    for _, c := range cases {
+        if c.in != c.want {
+            t.Fail()
+        }
+            
+    }
 }
